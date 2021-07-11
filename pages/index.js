5085a62a90a8b2eed95b51f5942b83b55b08b822
MahmudJion <mahmudh.jion02@gmail.com>
2021-07-11 18:54:02 +0600

    update branch

---
+++ pages/index.js
@@ -0,0 +1,55 @@
+import { Route, Link, Switch } from "react-router-dom";
+
+function Index () {
+    return <h2>Home</h2>;
+}
+
+function About () {
+    return <h2>About</h2>;
+}
+
+function Users () {
+    return <h2>Users</h2>;
+}
+
+function People () {
+    return <h2>People</h2>;
+}
+
+function NotFound () {
+    return <h2>Not found</h2>;
+}
+
+function App () {
+    return (
+        <div>
+            <h1>Welcome to Next.js!</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about/">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/users/">Users</Link>
+                    </li>
+                    <li>
+                        <Link to="/people/">People</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Switch>
+                <Route path="/" exact component={Index} />
+                <Route path="/about/" component={About} />
+                <Route path="/users/" component={Users} />
+                <Route path="/people/" component={People} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    );
+}
+
+export default App;
